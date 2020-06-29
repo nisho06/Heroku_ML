3dc63678f2b2feca7ecf84311a445cd8aeb402e2
nisho06 <47827247+nisho06@users.noreply.github.com>
2020-06-29 10:40:56 +0530

    Update app.py

--- Machine learning Model/app.py
+++ Machine learning Model/app.py
@@ -1,29 +1,53 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+import urllib
+import json
+import os
+import  pandas as pd
+
+from flask import Flask
+from flask import request
+from flask import make_response
 import pickle
 
 app= Flask(__name__)
 model = pickle.load(open("model.pkl","rb"))
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+        req = request.get_json(silent=True, force=True)
+        res = json.dumps(req['queryResult']['parameters'], indent=4)
+        int_features=json.loads(res)
+        print(int_features)
+        final_features=pd.DataFrame({'age':[int(int_features['age']['amount'])],'num_preg':[int(int_features['num_preg'])],'glucose_conc':[int(int_features['glucose_conc'])],'diastolic_bp':[int(int_features['diastolic_bp'])],'insulin':[int(int_features['insulin'])],'bmi':[float(int_features['bmi'])],'diab_pred':[float(int_features['diab_pred'])],'skin':[float(int_features['skin'])]})
+        r=get_data(final_features)
+        print(r)
+        r=json.dumps(r)
+        result = make_response(r)
+        result.headers['Content-Type'] = 'application/json'
+        return result
 
-@app.route("/predict",methods=["POST"])
-def predict():
-    features=[float(x) for x in request.form.values()]
-    features_final=[np.array(features)]
-    prediction= model.predict(features_final)
-    if prediction == [1]:
-        prediction = "has diabetes"
-    else:
-        prediction = "has no diabetes"
+def get_data(final_features):
+   prediction=model.predict_proba(final_features)
+   pred=(prediction[0][0])
+   if (pred<0.5):
+        output='You have '+ str((1-pred)*100)+'%'+' possibility of having Diabetes.Please take immediate action to cure your self.'
+   elif (0.5<=pred<0.75):
+        output='You have '+ str((1-pred)*100)+'%'+' possibility of having Diabetes.You have to be careful and take some actions.'
+   elif (pred>0.75):
+        output='You have '+ str((1-pred)*100)+'%'+' possibility of having Diabetes.You are safe'
+ 
+    
+   return {
+       "fulfillmentText" : output,
+       "Prediction_message": "prediction"
+        
+   }
+ 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 80))
 
-    return render_template("index.html",pred="The person {}".format(prediction))
+    print ("Starting app on port %d" %(port))
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug=True, port=port, host='0.0.0.0')
     
